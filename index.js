4588f6c76a4c758cf8641f8ad3cd4b7d718cdf0e
Basheer Tome <basheer@basheertome.com>
2014-07-21 00:56:21 -0700

    Semi-final thumbnail layout and design finished

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var colorWheel = ['#F68C1F', '#55C0A5', '#EF4D4D', '#4d4846', '#93C84A', '#896749', '#514099', '#3C92CF', '#888888', 'transparent'];
-
 $(window).scroll(function() {
-	percent = Math.round($(window).scrollTop() / (($(document).height()-$(window).height())/7)*1.5) * 94;
+	var speed = 5;
+	percent = Math.round($(window).scrollTop() / (($(document).height()-$(window).height())/7)*speed) * 94;
 	offset = percent - (Math.floor(percent/846) * 846);
 	$('.horse').css({'background-position': 'center -' + offset + 'px'});
 });
