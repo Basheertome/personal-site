ed86e898a5823a90331aa80609fe364fa8df67f0
Basheer Tome <basheer@basheertome.com>
2014-07-24 01:06:51 -0700

    Dramatically changed top of homepage to gif animation, killed the horse and texture. Changed description. Re-mobilized.

--- Build/index.js
+++ Build/index.js
@@ -1,18 +1,5 @@
 $(document).ready(function(){
-	$('.horse').hover(function() {
-		$('.horse').css({'background-position': 'center -' + 188 + 'px'});
-	}, function() {
-		horse_scroll();
+	$(window).scroll(function(event) { 
+		$('.arrow').fadeOut();
 	});
 });
-
-$(window).scroll(function() {
-	horse_scroll();
-});
-
-function horse_scroll() {
-	var speed = 5;
-	percent = Math.round($(window).scrollTop() / (($(document).height()-$(window).height())/7)*speed) * 94;
-	offset = percent - (Math.floor(percent/846) * 846);
-	$('.horse').css({'background-position': 'center -' + offset + 'px'});
-}
