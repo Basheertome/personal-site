a8a8110bf1e4e9e0a8e3e4660e638d324d2cbaad
Basheer Tome <basheer@basheertome.com>
2014-07-27 14:19:48 -0700

    Changed top intro to fixed w/ fade vs aabsolute w/ scroll

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
+	if ('ontouchstart' in document.documentElement) {
+		$('html').addClass('touch');
+	}
 	$(window).scroll(function(event) { 
-		$('.arrow').fadeOut();
+		percent = 1 - (($(window).scrollTop() / $('.intro').height()*2));
+		$('.intro').css({'opacity': percent});
 	});
 });
