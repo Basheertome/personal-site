ed7c97ec5c3a8c342fb98041b807e2f072ffb612
Basheer Tome <basheer@basheertome.com>
2014-08-02 20:14:52 -0700

    Added photography link to homepage. Added Instinct thumbnail. Resized and positions thumbs. Made links going back to homepage skip down to projects.

--- Build/index.js
+++ Build/index.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
 
+	if (window.location.hash.split('#')[1] != undefined) {
+		window.location.replace("#");  
+		if (typeof window.history.replaceState == 'function') {
+		  history.replaceState({}, '', window.location.href.slice(0, -1));
+		}
+		$(window).scrollTop($('.intro').height()-100);
+	}
+
+	$('.intro').animate({'opacity': 1}, 1000);
+
 	if ('ontouchstart' in document.documentElement) {
 		$('html').addClass('touch');
 	}
 
 	$(window).scroll(function(event) { 
 		percent = 1 - (($(window).scrollTop() / $('.intro').height()*2));
-		$('.intro').css({'opacity': percent});
+		if (percent >= 1) {
+			$('.intro').css({'opacity': 1});
+		} else if (percent >= 0) {
+			$('.intro').css({'opacity': percent});
+		} else { $('.intro').css({'opacity': 0}); }
 	});
 
 	hoverx = new Array($('.thumbnails').find('a').length);
