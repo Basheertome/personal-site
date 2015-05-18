75d1d51bc74776590c009ce537b3dc5aba26a489
Basheer Tome <basheer@basheertome.com>
2015-05-18 14:09:51 -0400

    Swapped header nav underline colors and removed animation from resume hover effect

--- Build/28ee8994105fd1d03732c5a1356c4b23.js
+++
@@ -1,2 +0,0 @@
-$.getScript("http://stats.basheertome.com/?js");var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-25940549-1"]),_gaq.push(["_setDomainName","basheertome.com"]),_gaq.push(["_trackPageview"]),function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"==document.location.protocol?"https://":"http://")+"stats.g.doubleclick.net/dc.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}();
-;
