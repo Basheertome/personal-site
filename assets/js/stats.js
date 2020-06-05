e40e2c9fc0d758dd6fbd153f4833d303c859e403
Basheer Tome <basheer@basheertome.com>
2020-06-05 13:58:15 -0700

    Switch analytics to Fathom

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,1 +1,15 @@
-$.getScript('https://stats.basheer.co/?js');
+<!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
+<script>
+(function(f, a, t, h, o, m){
+	a[h]=a[h]||function(){
+		(a[h].q=a[h].q||[]).push(arguments)
+	};
+	o=f.createElement('script'),
+	m=f.getElementsByTagName('script')[0];
+	o.async=1; o.src=t; o.id='fathom-script';
+	m.parentNode.insertBefore(o,m)
+})(document, window, '//stats.basheer.co/tracker.js', 'fathom');
+fathom('set', 'siteId', 'OTTTC');
+fathom('trackPageview');
+</script>
+<!-- / Fathom -->
